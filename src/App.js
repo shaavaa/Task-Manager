8176f7260c58742fcde778d5f20b10714a4e3d11
shaavaa <darreelshava@gmail.com>
2024-09-03 09:03:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing Components
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+// Importing React Hooks
+import { useState, useEffect } from 'react';
+// Importing Packages
+import { v4 as uuidv4 } from 'uuid';
+import Swal from "sweetalert2";
 
 function App() {
+  // All States
+  const [loading, setloading] = useState(false); 
+  const [tasks, setTasks] = useState([]); // Task State
+  const [showAddTask, setShowAddTask] = useState(false); 
+  // Pre-loader
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false);
+    }, 3500);
+  }, [])
+  // Fetching from Local Storage
+  const getTasks = JSON.parse(localStorage.getItem("taskAdded"));
+  useEffect(() => {
+    if (getTasks == null) {
+      setTasks([])
+    } else {
+      setTasks(getTasks);
+    }
+  }, [])
+  // Add Task
+  const addTask = (task) => {
+    const id = uuidv4();
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask]);
+    Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully added a new task!'
+    })
+    localStorage.setItem("taskAdded", JSON.stringify([...tasks,
+      newTask]));
+  }
+  // Delete Task
+  const deleteTask = (id) => {
+    const deleteTask = tasks.filter((task) => task.id !== id);
+    setTasks(deleteTask);
+    Swal.fire({
+      icon: 'success',
+      title: 'Oops...',
+      text: 'You have successfully deleted a task!'
+    })
+    localStorage.setItem("taskAdded",
+      JSON.stringify(deleteTask));
+  }
+  // Edit Task
+  const editTask = (id) => {
+    const text = prompt("Task Name");
+    const day = prompt("Day and Time");
+    let data = JSON.parse(localStorage.getItem('taskAdded'));
+    const myData = data.map(x => {
+      if (x.id === id) {
+        return {
+          ...x,
+          text: text,
+          day: day,
+          id: uuidv4()
+        }
+      }
+      return x;
+    })
+    Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully edited an existing task!'
+    })
+    localStorage.setItem("taskAdded", JSON.stringify(myData));
+    window.location.reload();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+        <div className="container">
+          {/* App Header that has open and App Name */}
+          <Header showForm={() =>
+            setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
+          {/* Revealing of Add Task Form */}
+          {showAddTask && <AddTask onSave={addTask} />}
+          {/* Task Counter */}
+          <h3>Number of Tasks: {tasks.length}</h3>
+          {/* Displaying of Tasks */}
+          {
+            tasks.length > 0 ?
+              (<Tasks tasks={tasks}
+          onDelete={deleteTask} onEdit={editTask} />) :
+          ('No Task Found!')
+ }
+        </div>
+      }
+    </>
+  )
 }
-
 export default App;
